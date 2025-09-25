41ae4d7691547398c6bc2c7f56db2f0e25d36f3f
Julio <132522218+juliodf24@users.noreply.github.com>
2025-09-25 17:28:59 +0000

    criacao da pagina inicial

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    printf("---------------------------------\n");
+    printf("        Gerador de senha     \n");
+    printf("---------------------------------\n\n");
+    printf("1- gerar senha de 4 caracteres\n");
+    printf("2- gerar senha de 8 caracteres\n");
+    printf("3- gerar senha de 16 caracteres\n\n");
+    printf("---------------------------------\n\n");
+    return 0;
+}
